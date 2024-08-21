bd4c1f1c73c575a8fe4e8c8dce1c9813e7d36f0e
Alex <alexhwebdev@gmail.com>
2024-08-20 15:20:09 -1000

    Finished adding Likes functionality

--- api/controllers/like.js
+++ api/controllers/like.js
@@ -1,3 +1,55 @@
-export const getLike = (req, res) => {
-  res.send("getLike")
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getLikes = (req, res) => {
+  console.log('getLikes req ', req.query)
+  const q = "SELECT userId FROM likes WHERE postId = ?";
+
+  db.query(q, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json(err)
+    console.log('db.query data ', data)
+    return res.status(200).json(data.map(like => like.userId))
+  });
+
 }
+
+export const addLike = (req, res) => {
+  console.log('addLike req ', req)
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  // If token exists, validate it
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO likes (`userId`, `postId`) VALUES (?)";
+    const values = [
+      userInfo.id,
+      req.body.postId
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err)
+      return res.status(200).json("Post has been liked.")
+    });
+  })
+}
+
+export const deleteLike = (req, res) => {
+  console.log('deleteLike req ', req)
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  // If token exists, validate it
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "DELETE FROM likes WHERE `userId` = ? AND `postId` = ?";
+
+    db.query(q, [userInfo.id, req.query.postId], (err, data) => {
+      if (err) return res.status(500).json(err)
+      return res.status(200).json("Like removed.")
+    });
+  })
+}
+
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,15 +6,51 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import moment from "moment";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import { AuthContext } from "../../context/authContext";
+
 
 const Post = ({ post }) => {
-  console.log('post ', post)
+  // console.log('post ', post)
   const [commentOpen, setCommentOpen] = useState(false);
 
-  //TEMPORARY
-  const liked = false;
+  const {currentUser} = useContext(AuthContext);
+  console.log('currentUser ', currentUser)
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['likes', post.id],
+    queryFn: () =>
+      makeRequest.get("/likes?postId=" + post.id).then((res) => {
+        return res.data;
+      })
+  })
+  console.log('data ', data)
+
+  const queryClient = useQueryClient();
+
+  // POST text
+  // With useMutation, we are gonna make post request and if successful, refetch our posts in home page. So after adding new post, it will immediately refresh fetch methods and show post
+  const mutation = useMutation({
+    mutationFn: (liked) => {
+      console.log('liked ', liked)
+
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", {postId: post.id});
+    },
+    // https://tanstack.com/query/latest/docs/reference/QueryClient/#queryclientinvalidatequeries
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['likes'] // refresh posts query
+      })
+    } 
+  });
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  }
 
   return (
     <div className="post">
@@ -43,8 +79,27 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 Likes
+            {/* 12 Likes */}
+            {
+              isPending 
+                ? "Loading..."
+                : data.includes(currentUser.id) 
+                  ? <FavoriteOutlinedIcon 
+                      style={{color: 'red'}} 
+                      onClick={handleLike}
+                    /> 
+                  : <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            }
+            {/* {
+              data.includes(currentUser.id) 
+                ? <FavoriteOutlinedIcon style={{color: 'red'}} /> 
+                : <FavoriteBorderOutlinedIcon />
+            } */}
+            {/* { data.length } Likes */}
+            {
+              data ? data.length + " Likes" : ""
+            }
+
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
